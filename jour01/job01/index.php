026b3905a9a438c94a77f07f2e0bc729ceb2d6e9
YoussefGhollamallah <youssef.gh@live.fr>
2024-06-04 10:10:04 +0200

    job1 terminé

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,9 +1,29 @@
 <?php
 
-$_variable = "LaPlateforme";
+$_variable = "LaPlateforme ";
 
 echo $_variable;
 
-$_nouvelle_variable = "Vive";
+$_nouvelle_variable = "Vive ";
 $_troisieme_variable = "!";
 
+echo "<br>";
+
+echo $_nouvelle_variable , $_variable , $_troisieme_variable;
+
+$_number = 6;
+
+echo "<br>";
+echo $_number;
+echo "<br>";
+echo $_number + 4;
+
+echo "<br>";
+$_isBool = True;
+
+echo $_isBool;
+echo "<br>";
+
+$_isBool = False; 
+
+echo $_isBool;
